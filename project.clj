6b6e34cb9c624e0f14ccb3404f3522c48eccb9c0
Rahul De <rahul080327@gmail.com>
2021-03-08 14:57:22 +0100

    add lein

---
+++ project.clj
@@ -0,0 +1,12 @@
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+; which can be found in the file epl-v10.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+
+(defproject org.clojars.lispyclouds/navi "0.0.1"
+  :author       "Rahul De <rahul@mailbox.org>"
+  :url          "https://github.com/lispyclouds/navi"
+  :description  "A tiny library converting OpenAPI route definitions to Reitit routes."
+  :license      {:name "EPL 1.0"
+                 :url  "http://opensource.org/licenses/eclipse-1.0.php"}
+  :dependencies [[io.swagger.parser.v3/swagger-parser "2.0.24"]])
