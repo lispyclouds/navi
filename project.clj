9dcae354d0b66b80f8dc97350fa1c3fd524e6a66
Rahul De <rahul080327@gmail.com>
2022-07-18 07:35:17 +0100

    deps

--- project.clj
+++ project.clj
@@ -9,4 +9,4 @@
   :description  "A tiny library converting OpenAPI route definitions to Reitit routes."
   :license      {:name "EPL 1.0"
                  :url  "http://opensource.org/licenses/eclipse-1.0.php"}
-  :dependencies [[io.swagger.parser.v3/swagger-parser "2.0.30"]])
+  :dependencies [[io.swagger.parser.v3/swagger-parser "2.1.1"]])
